8ee9e5b684c12c0fc0a685c9b0e71874b2d7bc66
Vy Ngo Chi <ncv.ngovy@gmail.com>
2026-06-08 23:08:30 +0700

    Add initial project configuration and Gluestack UI provider components
    - Created .idea configuration files for project settings, including device manager, markdown, misc, modules, and VCS.
    - Added .npmrc file to manage npm settings with legacy-peer-deps enabled.
    - Implemented Babel configuration for Expo and NativeWind integration.
    - Set up Metro configuration for NativeWind support.
    - Created TypeScript declaration files for NativeWind and React Native CSS.
    - Developed Gluestack UI provider components for both web and native environments, including theme management and color scheme handling.
    - Added a script for managing color modes in the web environment.

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = function (api) {
+  api.cache(true);
+
+  return {
+    presets: [["babel-preset-expo"], "nativewind/babel"],
+
+    plugins: [
+      [
+        "module-resolver",
+        {
+          root: ["./"],
+
+          alias: {
+            "@/assets": "./assets",
+            "@": "./src",
+            "tailwind.config": "./tailwind.config.js",
+          },
+        },
+      ],
+      "react-native-worklets/plugin",
+    ],
+  };
+};
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,19 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import { useColorScheme } from 'react-native';
+import { DarkTheme, DefaultTheme, ThemeProvider } from "expo-router";
+import { useColorScheme } from "react-native";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
+import { AnimatedSplashOverlay } from "@/components/animated-icon";
+import AppTabs from "@/components/app-tabs";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import "./../global.css";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <GluestackUIProvider mode="system">
+      <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+        <AnimatedSplashOverlay />
+        <AppTabs />
+      </ThemeProvider>
+    </GluestackUIProvider>
   );
 }
