7579c2a82f182d63a2316466928ab4ea64f7b1f8
Vy Ngo Chi <ncv.ngovy@gmail.com>
2026-06-10 15:29:01 +0700

    feat: initialize project configuration and styling

---
+++ metro.config.js
@@ -0,0 +1,10 @@
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
+
+const config = getDefaultConfig(__dirname);
+config.resolver.sourceExts = [...config.resolver.sourceExts, "mjs"];
+
+module.exports = withNativeWind(config, {
+  input: "./global.css",
+  inlineRem: 16,
+});
